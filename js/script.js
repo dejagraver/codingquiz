230fb3df2fbff0f70df55cdacfb46d7181441621
dejagraver <dejaamaartin@gmail.com>
2021-03-21 17:54:11 -0400

    Redo Arrays with objects

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 //Start Quiz Button
 
 //create your (array full of quetions )objects 
-var questions = [questionone.q, questiontwo.q, questiontwo.q, questionthree.q, questionfour.q, questionfive.q];
+var questions = [
+    {
+        q: "Commonly used data types do not include ___.", 
+        a: "Strings",
+        options: ["1. Strings",  "2. Booleans", "3. Alerts", "4. Numbers"]
+    },
 
-var questionone =  {q: "Commonly used data types do not include ___.", a: "Strings"}
+    {
+        q: "The condition of an if / else statement is enclosed with ___.", 
+        a: "Curly Brackets",
+        options: ["1.Quotes", "2. Curly brackets", "3. Parenthesis", "4. Square brackets"]
+    },
+    {
+        q:"Arrays in javaScript can be used to store ___.", 
+        a: "All of the Above",
+        options: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"]
+    },
+    {
+        q: "String values must be enclosed within ___ when being assigned to variables.", 
+        a: "Quotes",
+        options: ["1. Commas", "2. Curly brackets", "3. Quotes", "4. Parenthesis"]
+    },
+    {
+        q: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+        a: "Console.log",
+        options: ["1. Javascript",  "2. Terminal/bash", "3. For loops", "4. Console.log"]
+    }
+]
 
-var questiontwo = {q: "The condition of an if / else statement is enclosed with ___.", a: "Curly Brackets"}
+var currentIndex = 0
+var score = [];
 
-var questionthree = {q:"Arrays in javaScript can be used to store ___.", a: "All of the Above"}
-
-var questionfour = {q: "String values must be enclosed within ___ when being assigned to variables.", a: "Quotes"}
-
-var questionfive = {q: "A very useful tool used during development and debugging for printing content to the debugger is:", a: "Console.log"} 
 //Click Start Quiz button
 //countdown function timer for the questions to be answered with in 
 //when countdown stops so does the quiz
@@ -32,137 +53,68 @@ function startTimer(duration, display) {
             clearInterval(timer);
         }
     }, 1000);
-    return;
 }
-
 window.onclick  = function () {
     var oneMinute = 60 * 1,
         display = document.querySelector('#time');
     startTimer(oneMinute, display);
-
 };
 
-//Click start button
-startQuiz () {
-    this.mainElement = document.querySelector('main');
 
-}
-
-
-// this.startButton = document.querySelector('Start Quiz');
-// this.quiz = { };
-// this.startButton.addEventListener('click', this.startQuiz.bind(this));
-
+// Click start button
+// startQuiz () {
+//     this.mainElement = document.querySelector('.quiz');
+//     this.startButton = document.querySelector('#start');
+//     this.quiz = { };
+//     this.startButton.addEventListener('click', this.startQuiz.bind(this));
+// }
+//when answer is clicked , validate when its click, checking it it right or wrong, incresing index to that next on to the next one  
+//javascript index, how you access items with in an array 
 //function that creates DOM elements for each questions asked 
 //create container to hold Question and Answer elements
-var createQuestionOne = function (questionId) {
+
+
+var createQuestions = function (questionId) {
     //create question by calling on object declared .q
-    var questionOneEl = document.createElement ("div");
+    var questionsDiv = document.createElement ("div");
     console.log(div)
-    questionOneEl.textContent = questionone.q;
-    questionOneEl.className = "question one"
-    answerOneEl.setAttribute("question-id", questionId);
-    actionContainerEl.appendChild(questionOneEl);
+        questionsDiv.textContent = questions[currentIndex].q;
+        questionsDiv.className = "questionEL"
+        answerOneEl.setAttribute("question-id", questionId);
+        actionContainerEl.appendChild(questionsDiv);
 
-    //create answer options button 
-    var answerOneEl = document.createElement ("button");
-    answerOneEl.textContent = "1. Strings",  "2. Booleans", "3. Alerts", "4. Numbers";
-    answerOneEl.className = "btn one";
-    answerOneEl.setAttribute("question-id", questionId);
-    actionContainerEl.appendChild(answerOneEl);
+//access the object 
+//for each of these things create a button and append it to the options 
+//create answer options button 
+    var optionsButton = document.createElement ("button");
+        optionsButton.textContent = options[currentI]
+        optionsButton.className = "optionsEL";
+        options.setAttribute("question-id", questionId);
+        actionContainerEl.appendChild(optionsButton);
+
+//onlclick the current q is being set , it will increment an index/number, then the new increment goes t
+options.onclick  = function () {
+  currentIndex++ 
+};
 }
 
-var createQuestionTwo = function (questionId) {
-    //create question 
-    var questionTwoEl = document.createElement ("div");
-    questionTwoEl.textContent = questiontwo.q;
-    questionTwoEl.className = "question two"
-    answerTwoEl.setAttribute("question-id", questionId);
-    actionContainerEl.appendChild(questionTwoEl);
 
-    //create answer options button
-    var answerTwoEl = document.createElement ("button");
-    answerTwoEl.textContent = "1.Quotes", "2. Curly brackets", "3. Parenthesis", "4. Square brackets";
-    answerTwoEl.className = "btn two";
-    answerTwoEl.setAttribute("question-id", questionId);
-    actionContainerEl.appendChild(answerTwoEl);
-}
-
-var createQuestionThree = function (questionId) {
-    //create question 
-    var questionThreeEl = document.createElement ("div");
-    questionThreeEl.textContent = questionthree.q;
-    //create answer options button
-    var answerThreeEl = document.createElement ("button");
-    answerThreeEl.textContent = "1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above";
-    answerThreeEl.className = "btn three";
-    answerThreeEl.setAttribute("question-id", questionId);
-    answerContainerEl.appendChild(answerThreeEl); 
-}
-
-var createQuestionFour = function (questionId) {
-    //create question 
-    var questionFourEl = document.createElement ("div");
-    questionFourEl = questionfour.q;
-
-    //create answer options button
-    var answerFourEl = document.createElement ("button");
-    answerFourEl.textContent = "1. Commas", "2. Curly brackets", "3. Quotes", "4. Parenthesis";
-    answerFourEl.className = "btn four";
-    answerFourEl.setAttribute("question-id", questionId);
-    answerContainerEl.appendChild(answerFourEl); 
-}
-
-var createQuestionFive = function (questionId) {
-    //create question 
-    var questionFiveEl = document.createElement ("div");
-    questionFiveEl = questionfive.q;
-    
-    //create answer options button
-    var answerFiveEl = document.createElement ("button");
-    answerFiveEl.textContent = "1. Javascript",  "2. Terminal/bash", "3. For loops", "4. Console.log";
-    answerFiveEl.className ="btn five";
-    answerFiveEl.setAttribute("question-id", questionId);
-    answerFiveEl.appendChild(answerFiveEl); 
-}
-
+// get mycurrent question is question at index 
+// for loop to create all the choices 
+// and append that to the final answer 
+//currentQuestions.options.foreach answer true or false and increment on to the next index 
 //when the answer is selected create a function that will cause the function to move onto the next one 
 //inside the function call on the choices function that increments points 
-
-
 // function check answer and create a for loop that willl call on the choices functions in the var choices
 //input and
 //function choices = the points earned for right answers
+
 function choices() {
     var score = 0; 
     for (i = 0; i < arr.length; i++) {
         var answer = confirm(arr[i].q)
     }
-    if (answer === questionone.a){
-        score++;
-        alert ('You are Correct!')
-    } else {
-        alert ('You are Wrong!')
-    }
-    if (answer === questiontwo.a){
-        score++;
-        alert ('You are Correct!')
-    } else {
-        alert ('You are Wrong!')
-    }
-    if (answer === questionthree.a){
-        score++;
-        alert ('You are Correct!')
-    } else {
-        alert ('You are Wrong!')
-    }
-    if (answer === questionfour.a){
-        score++;
-        alert ('You are Correct!')
-    } else {
-        alert ('You are Wrong!')
-    }
-    if (answer === questionfive.a){
+    if (answer === questions.a){
         score++;
         alert ('You are Correct!')
     } else {
@@ -172,10 +124,13 @@ function choices() {
 }
 saveScore();
 
+
+
+
+
 var saveScore = function() {
     localStorage.setItem("score", JSON.stringify(score));
   };
-
 
 //Show ScoreBoard with initials and score logged populated 
 var loadScore = function() {
@@ -184,4 +139,5 @@ var loadScore = function() {
     if (!savedTasks) {
       return false;
     }
-    // saveScore = JSON.parse(score);
+}
+// saveScore = JSON.parse(score)
