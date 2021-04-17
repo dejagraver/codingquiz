3b8ad019542a95a11ce8b53c47d02c4e48299888
dejagraver <dejaamaartin@gmail.com>
2021-04-17 11:43:18 -0400

    Building elements and queryselectors

--- js/script.js
+++ js/script.js
@@ -1,158 +1,91 @@
-
 //create your (array full of quetions )objects 
 var questions = [
     {
         q: "Commonly used data types do not include ___.", 
-        a: "Strings",
+        a: 1,
         options: ["1. Strings",  "2. Booleans", "3. Alerts", "4. Numbers"]
     },
 
     {
         q: "The condition of an if / else statement is enclosed with ___.", 
-        a: "Curly Brackets",
+        a: 2,
         options: ["1.Quotes", "2. Curly brackets", "3. Parenthesis", "4. Square brackets"]
     },
     {
         q:"Arrays in javaScript can be used to store ___.", 
-        a: "All of the Above",
+        a: 4,
         options: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"]
     },
     {
         q: "String values must be enclosed within ___ when being assigned to variables.", 
-        a: "Quotes",
+        a: 3,
         options: ["1. Commas", "2. Curly brackets", "3. Quotes", "4. Parenthesis"]
     },
     {
         q: "A very useful tool used during development and debugging for printing content to the debugger is:", 
-        a: "Console.log",
+        a: 4,
         options: ["1. Javascript",  "2. Terminal/bash", "3. For loops", "4. Console.log"]
     }
 ]
 
-
+// Set values for the functions 
+var currentQuestion = 0;
 var score = 0;
 var timer;
 var timeRemaining = 0;
-var score = 0;
+var highScore = [];
 
-//Start Quiz Button
+//Selecting the Quiz divs for information to be input with in 
+var codingQuizDiv = document.querySelector(".quiz");
+var scoreBoardDiv = document.querySelector(".time-score")
+
+///create elements
+var startQuizPage = document.createElement("div");
+var questionDisplayed = document.createElement("div");
+var answerButton = document.createElement("div");
+var showHighScore = document.createElement("label");
+var enterHighScore = document.createElement("input");
+var submitButton = document.createElement ("button")
+
+
+//Timer for when you start the game 
 function startQuiz() {
-    timeRemaining = 60;
-    document.getElementById("time").innerHTML = timeRemaining;
+  
+    }, 
+    //clear timer
 
-    timer = setInterval(function() {
-        timeRemaining--;
-        document.getElementById("time").innerHTML = timeRemaining;
+    //function that will
+function createSkeleton {
 
-        if (timeRemaining <= 0) {
-            clearInterval(timer);
-            endQuiz();
-        }
-    }, 1000);
-    next();
+}
+//have a function solely for displaying the questions that have been stored in the object above 
+//grab current question and update the different portions of that div as questions are answered 
+//
+function displayQuestions() {
+    var currentQuestion 
+}
+//clear out any of the previous questions option
+//once thats done you need to loop over to the
+
+//create a div for the questions to display 
+//once questions are displayed
+//you clear the function 
+
+function checkAnswers() {
+
 }
 
+//Timer for when you end the game 
 function endQuiz() {
     clearInterval(timer);
-
-}
-let question = document.getElementById("question");
-let options = Array.from(documenet.getElementByClassName("options-text"))
-
-
-var allQuestions = {};
-//Confirmation of choice selection
-var correctAnswers = true;
-var currentIndex = 0;
-//What question you are currently iterating on
-var questionLevel = 0;
-var allQuestions = {};
-var correctAnswers = true;
-var score = 0;
-var questionLevel = 0;
-var availableQuestions = [];
-//Click Start Quiz button
-//countdown function timer for the questions to be answered with in 
-//when countdown stops so does the quiz
-
-
-// Click start button
-// startQuiz () {
-//     this.mainElement = document.querySelector('.quiz');
-//     this.startButton = document.querySelector('#start');
-//     this.quiz = { };
-//     this.startButton.addEventListener('click', this.startQuiz.bind(this));
-// }
-//when answer is clicked , validate when its click, checking it it right or wrong, incresing index to that next on to the next one  
-//javascript index, how you access items with in an array 
-//function that creates DOM elements for each questions asked 
-//create container to hold Question and Answer elements
-
-
-var createQuestions = function (questionId) {
-    //create question by calling on object declared .q
-    var questionsDiv = document.createElement ("div");
-    console.log(div)
-        questionsDiv.textContent = questions[currentIndex].q;
-        questionsDiv.className = "questionEL"
-        answerOneEl.setAttribute("question-id", questionId);
-        actionContainerEl.appendChild(questionsDiv);
-
-//access the object 
-//for each of these things create a button and append it to the options 
-//create answer options button 
-    var optionsButton = document.createElement ("button");
-        optionsButton.textContent = options[currentI]
-        optionsButton.className = "optionsEL";
-        options.setAttribute("question-id", questionId);
-        actionContainerEl.appendChild(optionsButton);
-
-//onlclick the current q is being set , it will increment an index/number, then the new increment goes t
-options.onclick  = function () {
-  currentIndex++ 
-};
+// overview of the quiz results where youll input the name tag
+// followed by a onclick the button html tag
 }
 
 
-// get mycurrent question is question at index 
-// for loop to create all the choices 
-// and append that to the final answer 
-//currentQuestions.options.foreach answer true or false and increment on to the next index 
-//when the answer is selected create a function that will cause the function to move onto the next one 
-//inside the function call on the choices function that increments points 
-// function check answer and create a for loop that willl call on the choices functions in the var choices
-//input and
-//function choices = the points earned for right answers
+//getting the object of questions and asnwers into my page 
+//document.getelemnt by id/queryselctor etc... pop in the object value 
 
-function choices() {
-    var score = 0; 
-    for (i = 0; i < arr.length; i++) {
-        var answer = confirm(arr[i].q)
-    }
-    if (answer === questions.a){
-        score++;
-        alert ('You are Correct!')
-    } else {
-        alert ('You are Wrong!')
-    }
-    return score.join("")
-}
-saveScore();
+//store high score using set item in localstorage 
 
-
-
-
-
-var saveScore = function() {
-    localStorage.setItem("score", JSON.stringify(score));
-  };
-
-//Show ScoreBoard with initials and score logged populated 
-var loadScore = function() {
-    var savedTasks = localStorage.getItem("score");
-    // if there are no tasks, set tasks to an empty array and return out of the function
-    if (!savedTasks) {
-      return false;
-    }
-}
-// saveScore = JSON.parse(score)
+//Score board display
